16bd195016d73455d427d030e3344247c9e1f993
Christian Blades <christian.blades+github@gmail.com>
2020-09-20 20:08:57 -0400

    shell integrations, tmux, github

--- personal_git.nix
+++ personal_git.nix
@@ -5,6 +5,9 @@
       enable = true;
       userName = "Christian Blades";
       userEmail = builtins.concatStringsSep "@" [ "christian.blades+github" "gmail.com" ];
+      extraConfig = {
+        github.user = "christian-blades-cb";
+      };
     };
   };
 }
--- usual_setup.nix
+++ usual_setup.nix
@@ -4,6 +4,16 @@ with pkgs.lib;
 let
   emacsPkg = if pkgs.stdenv.isDarwin then pkgs.emacsMacport else pkgs.emacs26;
   phpLanguageServer = import ./deps/php-language-server/default.nix { inherit pkgs; };
+  dracula = pkgs.tmuxPlugins.mkDerivation {
+    pluginName = "dracula";
+    version = "unstable-2020-09-20";
+    src = pkgs.fetchFromGitHub {
+      owner = "dracula";
+      repo = "tmux";
+      rev = "85ce8a8b4a4b8bfcc3614b2dd8345f30f5ea091b";
+      sha256 = "0k1f3chhmnv6f2022cj58j3nyv6ssaqadvnvcc6bzk0y9ha70gnl";
+    };
+  };
 in {
   services = optionalAttrs pkgs.stdenv.isLinux { lorri.enable = true; };
 
@@ -30,8 +40,36 @@ in {
       enableFishIntegration = true;
     };
 
-    direnv.enable = true;
-    starship.enable = true;
+    direnv = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    tmux = {
+      enable = true;
+      clock24 = true;
+      historyLimit = 10000;
+      plugins = with pkgs.tmuxPlugins; [
+        yank fzf-tmux-url
+        {
+          plugin = dracula;
+          extraConfig = ''
+            set -g @dracula-show-weather false
+            set -g @dracula-military-time true
+            set -g @dracula-cpu-usage true
+            set -g @dracula-ram-usage true
+            set -g @dracula-show-network false
+          '';
+        }
+
+      ];
+    };
+
     bat.enable = true;
     jq.enable = true;
     texlive.enable = true;
@@ -66,7 +104,8 @@ in {
     global
     ctags
     clang-tools
-  ] ++ optional stdenv.isDarwin lorri;
+  ] ++ optionals stdenv.isDarwin [ lorri reattach-to-user-namespace ]
+  ++ optional stdenv.isLinux xsel;
 
   home.file = {
     ".emacs.d/init.el".source = ./init.el;
