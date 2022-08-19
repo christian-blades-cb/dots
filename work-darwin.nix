1893747799444c0e454707661251e595e456b183
Christian Blades <blades@mailchimp.com>
2022-08-19 11:39:38 -0400

    more tools from homebrew

--- work-darwin.nix
+++ work-darwin.nix
@@ -7,6 +7,8 @@
       tap "rsg/tap", "https://git.rsglab.com/rsg/homebrew-tap.git"
       brew "rsg/tap/devtool"
       brew "rsg/tap/pando-cli"
+      brew "rsg/tap/twurl"
+      brew "rsg/tap/dsf-cli"
     '';
   };
 }
