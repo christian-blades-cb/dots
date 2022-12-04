4f5a8dec09c13a1576b0ee5a4b8b9b935f88b29e
Christian Blades <christian.blades+github@gmail.com>
2022-12-03 22:15:05 -0500

    discord is back on the menu

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?rev=30ec7dc6416c7b3d286d047ec905eaf857f712f9";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?rev=30ec7dc6416c7b3d286d047ec905eaf857f712f9";
     darwin.url = "github:lnl7/nix-darwin?rev=4182ad42d5fb5001adb1f61bec3a04fae0eecb95";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
@@ -66,7 +66,10 @@
 
     homeConfigurations = {
       parkour = let
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
       in
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
--- personal.nix
+++ personal.nix
@@ -1,17 +1,7 @@
 { config, pkgs, ...}:
-let
-  discord = pkgs.callPackage <nixpkgs/pkgs/applications/networking/instant-messengers/discord/base.nix> rec {
-    pname = "discord";
-    binaryName = "Discord";
-    desktopName = "Discord";
-    version = "0.0.14";
-    src = pkgs.fetchurl {
-      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "1rq490fdl5pinhxk8lkfcfmfq7apj79jzf3m14yql1rc9gpilrf2";
-    };
-  };
-in {
+{
+  nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-    # discord
+    discord
   ];
 }
