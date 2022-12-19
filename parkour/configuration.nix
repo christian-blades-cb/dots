4123095b7fcc2441b262a8f973894260b86bc633
Christian Blades <christian.blades+github@gmail.com>
2022-12-19 00:11:02 -0500

    hibernate instead of just running the battery down

--- parkour/configuration.nix
+++ parkour/configuration.nix
@@ -24,6 +24,12 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # hibernate after sleeping for a while
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=1h
+  '';
+  services.logind.lidSwitch = "suspend-then-hibernate";
+
   # Enable networking
   networking.networkmanager.enable = true;
 
