99e1be359513ff556246f9f4901027afe4ad82c2
Christian Blades <christian.blades+github@gmail.com>
2023-02-13 18:22:44 -0500

    is my memory okay?

--- parkour/configuration.nix
+++ parkour/configuration.nix
@@ -16,6 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.systemd-boot.memtest86.enable = true;
 
   networking.hostName = "parkour"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
